f64223cdb52bf17748e2f24f0c36a14796abf597
Nichola Wolfe <wolfe.nichola@gmail.com>
2018-05-15 20:33:44 +0000

    Added Javascript

---
+++ home/static/home/js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+$('#myForm').on('submit', function(e){
+  $('#myModal').modal('show');
+  e.preventDefault();
+
+ });
+  
+$('a').mouseenter(function() {
+ 	$(this).css("color", "#5EC5C4");
+ });
+
+$('a').mouseleave(function(){
+    $(this).css("color", "#4a4a4f");
+});
+
+$(".card").mouseenter(function(){
+    $(this).css("background-color", "#5EC5C4").css("color", "white");
+});
+
+$(".card").mouseleave(function(){
+    $(this).css("background-color", "white");
+});
+});
+
+
+
+
+
